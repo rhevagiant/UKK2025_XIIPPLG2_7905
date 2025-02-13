62d607dcc1ceb97acfb974ffeab5963e208a2564
rhevagiant <122246574+rhevagiant@users.noreply.github.com>
2025-02-13 10:51:09 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+
+
+export default function App() {
+
+
+  return (
+    <>
+      <Typography variant="body2" sx={{textAlign: "center"}}>ukk</Typography>
+    </>
+  )
+}
+
